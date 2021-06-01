8435bb3c6a3ffb534055396e535a193e3c8a3362
Ilya <il.v.matveev@yandex.ru>
2021-06-01 11:19:15 +0300

    фadd terraform fileаор

---
+++ astra-static-web-shop/main_s3_backet.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region ="us-east-1"
+}
+#---создание ресурса с приданием ему имени и списка контроля доступа - acl
+resource "aws_s3_bucket" "b25sdr4" {
+  bucket = "s3-website-html"
+  acl    = "public-read"
+/*  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }*/
+}
+#---политикa сегмента, которая делает его содержимое общедоступным
+resource "aws_s3_bucket_public_access_block" "static_web" {
+  bucket = aws_s3_bucket.b25sdr4.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}  
