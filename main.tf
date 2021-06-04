bbd51db5f843ad3a426c2561351064c6b1458a56
ildarbiano <76512541+ildarbiano@users.noreply.github.com>
2021-06-04 18:11:03 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 ###---конфигурация terraform и провайдеров---###
-#--создание места хранения backends .tfstate terraform {
-backend "s3" {            
-  bucket = "astra-simple-web-shop"
-  key = "s3-backup/tfstate"
+#--создание места хранения backends .tfstate 
+terraform {
+  backend "s3" {            
+    bucket = "astra-simple-web-shop"
+    key = "s3-backup/tfstate"
   }
 }
 provider "aws" {
