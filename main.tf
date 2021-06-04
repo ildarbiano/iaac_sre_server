52e2b8b9b5fda9c154681a8cb5667361071b6da9
ildarbiano <76512541+ildarbiano@users.noreply.github.com>
2021-06-04 18:04:21 +0300

    почистил комментарии в тексте

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
-###---main.tf----конфигурация terraform и провайдеров
-terraform {
-  backend "s3" {            #--создание места хранения backends .tfstate 
+###---конфигурация terraform и провайдеров---###
+#--создание места хранения backends .tfstate terraform {
+  backend "s3" {            
     bucket = "astra-simple-web-shop"
-#    region = "us-east-1"   
     key = "s3-backup/tfstate"
   }
 }
 provider "aws" {
     profile = "default"
-#    region = "us-east-1"
 }
