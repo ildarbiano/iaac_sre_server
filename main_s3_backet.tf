80d755d17096d07d3e71e8239ad8bc1e217a5d14
ildarbiano <76512541+ildarbiano@users.noreply.github.com>
2021-06-04 13:10:11 +0300

    внёс комментарии

--- main_s3_backet.tf
+++ main_s3_backet.tf
@@ -1,24 +1,25 @@
+###---main.tf----конфигурация terraform и провайдеров
 terraform {
-  backend "s3" {            #---#--создание места хранения .tfstate 
-    
+  backend "s3" {            #--создание места хранения backends .tfstate 
     bucket = "astra-simple-web-shop"
     region = "us-east-1"
     key = "s3-backup/tfstate"
   }
 }
-
 provider "aws" {
   region ="us-east-1"
 }
+
+###---s3.tf
 #---создание ресурса с приданием ему имени и списка контроля доступа - acl
 resource "aws_s3_bucket" "b25sdr4" {
   bucket = "s3-website-html"
   acl    = "public-read"
-/*  website {
+#  policy = aws_s3_bucket_policy.public_read   #---Действительный документ JSON политики сегмента
+  website {
     index_document = "index.html"
     error_document = "error.html"
-
-    routing_rules = <<EOF
+/*    routing_rules = <<EOF
 [{
     "Condition": {
         "KeyPrefixEquals": "docs/"
@@ -30,6 +31,7 @@ resource "aws_s3_bucket" "b25sdr4" {
 EOF
   }*/
 }
+
 #---политикa сегмента, которая делает его содержимое общедоступным
 resource "aws_s3_bucket_public_access_block" "static_web" {
   bucket = aws_s3_bucket.b25sdr4.id
