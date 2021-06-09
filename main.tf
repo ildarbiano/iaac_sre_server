dbaf6efc59971c64a899c815930fde3f19c8f6f0
ildarbiano <76512541+ildarbiano@users.noreply.github.com>
2021-06-09 20:18:18 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 ###---конфигурация terraform и провайдеров---###
 #--создание места хранения backends .tfstate 
-/*
 terraform {
   backend "s3" {            
     bucket = "astra-simple-web-shop"
-    region = var.availability_zone_names
+    region = var.aws-region
     key = "s3.tfstate"
   }
 }
-*/
+
 provider "aws" {
   profile = "default"
   region  = var.aws-region
